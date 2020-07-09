a994aeea7d68f2ca967604035a4ccaab435a1e19
frrist <forrest@protocol.ai>
2020-07-08 18:13:27 -0700

    feat: add deal state and proposal tracking to cw

--- cmd/lotus-chainwatch/sync.go
+++ cmd/lotus-chainwatch/sync.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/filecoin-project/specs-actors/actors/builtin/market"
 	"math"
 	"sync"
 	"time"
@@ -72,6 +73,20 @@ type minerStateInfo struct {
 	qalPower big.Int
 	ssize    uint64
 	psize    uint64
+}
+
+type marketStateInfo struct {
+	// common
+	act       types.Actor
+	stateroot cid.Cid
+
+	// calculating changes
+	// calculating changes
+	tsKey       types.TipSetKey
+	parentTsKey types.TipSetKey
+
+	// market actor specific
+	state market.State
 }
 
 type actorInfo struct {
@@ -304,35 +319,46 @@ func syncHead(ctx context.Context, api api.FullNode, st *storage, headTs *types.
 
 		log.Infof("Getting miner info")
 
+		// highly likely that the market actor will change at every epoch
+		marketActorChanges := make(map[types.TipSetKey]*marketStateInfo, len(changes))
+
 		minerChanges := 0
 		for addr, m := range actors {
 			for actor, c := range m {
-				if actor.Code != builtin.StorageMinerActorCodeID {
-					continue
-				}
-
-				// only want miner actors with head change events
+				// only want actors with head change events
 				if _, found := headsSeen[actor.Head]; found {
 					continue
 				}
-				minerChanges++
-
-				minerTips[c.tsKey] = append(minerTips[c.tsKey], &minerStateInfo{
-					addr:      addr,
-					act:       actor,
-					stateroot: c.stateroot,
-
-					tsKey:       c.tsKey,
-					parentTsKey: c.parentTsKey,
-
-					state: miner.State{},
-					info:  miner.MinerInfo{},
-
-					rawPower: big.Zero(),
-					qalPower: big.Zero(),
-				})
-
 				headsSeen[actor.Head] = struct{}{}
+
+				switch actor.Code {
+				case builtin.StorageMarketActorCodeID:
+					marketActorChanges[c.tsKey] = &marketStateInfo{
+						act:         actor,
+						stateroot:   c.stateroot,
+						tsKey:       c.tsKey,
+						parentTsKey: c.parentTsKey,
+						state:       market.State{},
+					}
+				case builtin.StorageMinerActorCodeID:
+					minerChanges++
+
+					minerTips[c.tsKey] = append(minerTips[c.tsKey], &minerStateInfo{
+						addr:      addr,
+						act:       actor,
+						stateroot: c.stateroot,
+
+						tsKey:       c.tsKey,
+						parentTsKey: c.parentTsKey,
+
+						state: miner.State{},
+						info:  miner.MinerInfo{},
+
+						rawPower: big.Zero(),
+						qalPower: big.Zero(),
+					})
+				}
+
 			}
 		}
 
@@ -392,6 +418,21 @@ func syncHead(ctx context.Context, api api.FullNode, st *storage, headTs *types.
 		})
 		log.Infow("Completed Miner Processing", "duration", time.Since(minerProcessingStartedAt).String(), "processed", minerChanges)
 
+		log.Info("Getting market actor info")
+		// TODO: consider taking the min of the array length and using that for concurrency param, e.g:
+		// concurrency := math.Min(len(marketActorChanges), 50)
+		parmap.Par(50, parmap.MapArr(marketActorChanges), func(mrktInfo *marketStateInfo) {
+			astb, err := api.ChainReadObj(ctx, mrktInfo.act.Head)
+			if err != nil {
+				log.Error(err)
+				return
+			}
+			if err := mrktInfo.state.UnmarshalCBOR(bytes.NewReader(astb)); err != nil {
+				log.Error(err)
+				return
+			}
+		})
+
 		log.Info("Getting receipts")
 
 		receipts := fetchParentReceipts(ctx, api, toSync)
@@ -439,6 +480,17 @@ func syncHead(ctx context.Context, api api.FullNode, st *storage, headTs *types.
 
 		log.Info("updating miner sectors heads")
 		if err := st.updateMinerSectors(minerTips, api); err != nil {
+			log.Error(err)
+			return
+		}
+
+		log.Info("Storing market actor info")
+		if err := st.storeMarketActorDealProposals(marketActorChanges, api); err != nil {
+			log.Error(err)
+			return
+		}
+
+		if err := st.storeMarketActorDealStates(marketActorChanges, api); err != nil {
 			log.Error(err)
 			return
 		}
